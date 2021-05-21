c62b18906d1650bac7b5d93db57ffe7d25940046
jHoon0223 <aleejaehun@naver.com>
2021-05-21 21:22:58 +0900

    210521 : test

--- src/Book.java
+++ src/Book.java
@@ -1,14 +1,30 @@
 public class Book {
-    int i = 0;
-    Book increase(){
-        i++;
-        return this;
+    String title;
+    String author;
+
+    void show() {
+        System.out.println(title + " " + author);
     }
-    void show(){
-        System.out.println(i);
+
+    public Book() {
+        this("", "");
+        System.out.println("생성자 호출됨");
     }
-    public static void main(String[] args){
-        Book tt = new Book();
-        tt.increase().increase().increase().show();
+
+    public Book(String title) {
+        this(title, "작자미상");
+    }
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public static void main(String[] args) {
+        Book littlePrince = new Book("어린왕자", "생택쥐페리");
+        Book love = new Book("춘향전");
+        Book empty = new Book();
+
+        love.show();
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,21 +1,25 @@
-import java.util.Scanner;
+class Shape {
+    protected String name;
+    public void paint(){
+        draw();
+    }
+    public void draw() {
+        System.out.println("Shape");
+    }
+}
 
-public class Test {
-    private String a;
-    private String b;
+public class Test extends Shape{
+    protected String name;
+    @Override
+    public void draw() {
+        name = "Circle";
+        super.name = "Shape";
+        super.draw();
+        System.out.println(name);
+    }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Test[] t = new Test[2];
-
-        int c = scanner.nextInt();
-
-        for (int i = 0; i < c; i++) {
-            t[i].a = scanner.next();
-            t[i].b = scanner.next();
-        }
-
-        System.out.println(c + t[0].a + t[0].b + t[1].a + t[1].b);
-
+        Shape b = new Test();
+        b.paint();
     }
 }
